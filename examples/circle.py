49cac5e6b42f3ca2208001cff943d21b37f4a25d
Stijn Koppen <s.koppen@tudelft.nl>
2023-02-22 18:01:11 +0100

    Add two examples: generates NURBS curves and plots them

---
+++ examples/circle.py
@@ -0,0 +1,17 @@
+from math import sqrt
+import numpy as np
+from nurbs import Curve, plot
+import matplotlib.pyplot as plt
+
+a = 1 / sqrt(2)
+
+knots = np.array([0, 0, 0, 0.25, 0.25, 0.5, 0.5, 0.75, 0.75, 1, 1, 1], dtype=float)
+
+ctrlpts = np.array([[0, -1], [-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]], dtype=float)
+
+weights = np.array([1, a, 1, a, 1, a, 1, a, 1], dtype=float)
+
+circle = Curve(ctrlpts.T, weights, 2, knots)
+
+plot(circle, frenet_serret=True)
+plt.show()
