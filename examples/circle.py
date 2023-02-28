81ec704f97280e751c8160bfee8a61f78ee3c000
Stijn Koppen <s.koppen@tudelft.nl>
2023-02-28 09:55:12 +0100

    Adds degree 3 circle example

--- examples/circle.py
+++ examples/circle.py
@@ -3,7 +3,8 @@ from math import sqrt
 import matplotlib.pyplot as plt
 import numpy as np
 
-from nurbs import Curve, plot
+from nurbs import Curve
+from plot_functions import plot
 
 a = 1 / sqrt(2)
 
@@ -15,5 +16,14 @@ weights = np.array([1, a, 1, a, 1, a, 1, a, 1], dtype=float)
 
 circle = Curve(2, ctrlpts.T, knots, weights)
 
+plot(circle, frenet_serret=True)
+plt.show()
+
+knots = np.array([0, 0, 0, 0, 0.5, 0.5, 0.5, 1, 1, 1, 1], dtype=float)
+ctrlpts = np.array([[1, 0], [1, 2], [-1, 2], [-1, 0], [-1, -2], [1, -2], [1, 0]],
+                                   dtype=float)
+a = 1/3
+weights = np.array([1, a, a, 1, a, a, 1], dtype=float)
+circle = Curve(3, ctrlpts.T, knots, weights)
 plot(circle, frenet_serret=True)
 plt.show()
